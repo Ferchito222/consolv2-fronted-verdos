9906c96060edfdb5459fdd9ea123adf6c1086a6a
dayvi2023 <deyvichanlcd@gmail.com>
2024-06-21 17:52:42 -0400

    se cambio el titulo de la pagina y se adecuo el grafico del login a un tamaño ideal

--- src/components/graphics/DoughnutChart.tsx
+++ src/components/graphics/DoughnutChart.tsx
@@ -1,5 +1,5 @@
-// src/graphics/DoughnutChart.tsx
 import React from 'react';
+import { Grid, Box, Typography, useMediaQuery, useTheme } from '@mui/material';
 import { Doughnut } from 'react-chartjs-2';
 import {
   Chart as ChartJS,
@@ -9,66 +9,69 @@ import {
   ChartOptions,
 } from 'chart.js';
 import { ChartData } from 'chart.js/auto';
-import { Box, Grid, Typography } from '@mui/material';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-const DoughnutChart: React.FC = () => {
-  const data: ChartData<'doughnut'> = {
-    labels: ['Uno', 'Dos', 'Tres', 'Cuatro'],
-    datasets: [
-      {
-        label: '# of Votes',
-        data: [12, 19, 3, 5],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-        ],
-        borderWidth: 1,
-      },
-    ],
-  };
-
-  const options: ChartOptions<'doughnut'> = {
-    responsive: true,
-    maintainAspectRatio: false,
-    plugins: {
-      legend: {
-        position: 'top',
-      },
-      tooltip: {
-        enabled: true,
-      },
+const data: ChartData<'doughnut'> = {
+  labels: ['Red', 'Blue', 'Yellow', 'Green'],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [12, 19, 3, 5],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+      ],
+      borderWidth: 1,
     },
-  };
+  ],
+};
+
+const options: ChartOptions<'doughnut'> = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    tooltip: {
+      enabled: true,
+    },
+  },
+};
+
+const MyComponent: React.FC = () => {
+  const theme = useTheme();
+  const isXs = useMediaQuery(theme.breakpoints.down('xs'));
+  const isSm = useMediaQuery(theme.breakpoints.down('sm'));
+  const isMd = useMediaQuery(theme.breakpoints.up('md'));
 
   return (
     <Grid container direction="column" alignItems="center" spacing={2} sx={{ width: '100%', height: '100%' }}>
       <Grid item>
-      <Typography variant="h4" align={'center'} sx={{ fontWeight: '600' }}>
-            Grafica de Donut
-          </Typography>
+        <Typography variant="h4" align="center" sx={{ fontWeight: 600 }}>
+          Gráfica de Recepciones
+        </Typography>
       </Grid>
-      <Grid item sx={{ width: '100%', height: '100%' }}>
+      <Grid item sx={{ width: { xs: '90%', sm: '70%', md: '40%' }, height: { xs: '50%', sm: '60%', md: '50%' } }}>
         <Box sx={{ width: '100%', height: '100%' }}>
           <Doughnut data={data} options={options} />
         </Box>
       </Grid>
       <Grid item>
-      <Typography variant="h6" align={'center'} sx={{ fontWeight: '600' }}>
-            Fuente de Datos - Consolidador SNIS
-          </Typography>
+        <Typography variant="h6" align="center" sx={{ fontWeight: 600 }}>
+          Sistema de Consolidación WEB
+        </Typography>
       </Grid>
     </Grid>
   );
 };
 
-export default DoughnutChart;
+export default MyComponent;
